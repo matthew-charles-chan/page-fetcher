9287f4e135bd289ab047a102c8213db7b7f29931
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-16 03:37:21 +0000

    handleing invalid url and filepaths

--- fetcher.js
+++ fetcher.js
@@ -9,8 +9,13 @@ const args = process.argv.slice(2)
 
 
 request(args[0], (error, response, body) => {
+  if (!response) {
+    console.log('URL Invalid')
+    return
+  }
+
   fs.writeFile(args[1], body, () => {
-    if (error) throw error
+    if (error) throw error;
     const stats = fs.statSync(args[1])
     console.log(`Downloaded and saved ${stats.size} bytes to ${args[1]}`)
   });
