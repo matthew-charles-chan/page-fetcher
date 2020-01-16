b168cdbf6b8f3dab69553c9a82a65c3478c9b7ca
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-16 03:13:12 +0000

    page-fetcher

---
+++ fetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const fs = require('fs');
+
+const args = process.argv.slice(2)
+
+
+
+
+
+request(args[0], (error, response, body) => {
+  fs.writeFile(args[1], body, () => {
+    if (error) throw error
+    const stats = fs.statSync(args[1])
+    console.log(`Downloaded and saved ${stats.size} bytes to ${args[1]}`)
+  });
+});
